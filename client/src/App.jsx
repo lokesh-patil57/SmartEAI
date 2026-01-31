e78fdaa3b06be819a7604f7ecf1bb11e5fd1c382
Lucky Patil <luckypatil577R@gmail.com>
2026-02-01 00:21:44 +0530

    Backent running

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
+import Editor from "./pages/Editor";
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/editor" element={<Editor />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+function Landing() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-main">
+      <h1 className="text-5xl font-extrabold mb-4 gradient-primary bg-clip-text text-transparent">
+        SmartEAI
+      </h1>
+      <p className="text-lg text-secondary">
+        Smart Ethical AI for Internship Applications
+      </p>
+    </div>
+  );
+}
+
+export default Landing;
